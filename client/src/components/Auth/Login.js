9a77541bddd33c68dcfcabf241fa3c8ca6fbc7b3
Kabir Maan <kabirmaan9@gmail.com>
2020-03-12 18:58:56 +0000

    Authenticate users from apollo server, login/create user from google account

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,13 +1,28 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import { GoogleLogin } from "react-google-login";
+import { GraphQLClient } from "graphql-request";
 // import Typography from "@material-ui/core/Typography";
 
+const ME_QUERY = `{
+    me{
+      _id
+      email
+      name
+      picture
+    }
+  }`;
+
 const Login = ({ classes }) => {
-  const onSuccess = googleUser => {
+  const onSuccess = async googleUser => {
     console.log(googleUser);
     const idToken = googleUser.getAuthResponse().id_token;
     console.log({ idToken });
+    const client = new GraphQLClient("http://localhost:4000/graphql", {
+      headers: { authorization: idToken }
+    });
+    const data = await client.request(ME_QUERY);
+    console.log({ data });
   };
 
   return (
