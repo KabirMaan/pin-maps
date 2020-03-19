36c995d452c009e54d784ebafc4de525bc964c5c
Kabir Maan <kabirmaan9@gmail.com>
2020-03-19 19:56:23 +0000

    Created and styled Blog component

--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -1,15 +1,65 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
-// import TextField from "@material-ui/core/TextField";
-// import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
-// import AddAPhotoIcon from "@material-ui/icons/AddAPhotoTwoTone";
-// import LandscapeIcon from "@material-ui/icons/LandscapeOutlined";
-// import ClearIcon from "@material-ui/icons/Clear";
-// import SaveIcon from "@material-ui/icons/SaveTwoTone";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import AddAPhotoIcon from "@material-ui/icons/AddAPhotoTwoTone";
+import LandscapeIcon from "@material-ui/icons/LandscapeOutlined";
+import ClearIcon from "@material-ui/icons/Clear";
+import SaveIcon from "@material-ui/icons/SaveTwoTone";
 
 const CreatePin = ({ classes }) => {
-  return <div>CreatePin</div>;
+  return (
+    <form className={classes.form}>
+      <Typography
+        className={classes.alignCenter}
+        component="h2"
+        variant="h4"
+        color="secondary"
+      >
+        <LandscapeIcon className={classes.iconLarge} /> Pin Location
+      </Typography>
+      <div>
+        <TextField name="title" label="title" placeholder="Insert Pin Title" />
+        <input
+          accept="image/*"
+          id="image"
+          type="file"
+          className={classes.input}
+        />
+        <label htmlFor="image">
+          <Button component="span" size="small" className={classes.button}>
+            <AddAPhotoIcon />
+          </Button>
+        </label>
+      </div>
+      <div className={classes.contentField}>
+        <TextField
+          name="content"
+          label="Content"
+          multiline
+          rows="6"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+        />
+      </div>
+      <div>
+        <Button className={classes.button} variant="contained" color="primary">
+          <ClearIcon className={classes.leftIcon} /> Discared
+        </Button>
+        <Button
+          type="submit"
+          className={classes.button}
+          variant="contained"
+          color="secondary"
+        >
+          Submit
+          <SaveIcon className={classes.rightIcon} />
+        </Button>
+      </div>
+    </form>
+  );
 };
 
 const styles = theme => ({
@@ -18,11 +68,11 @@ const styles = theme => ({
     justifyContent: "center",
     alignItems: "center",
     flexDirection: "column",
-    paddingBottom: theme.spacing.unit
+    paddingBottom: theme.spacing(1)
   },
   contentField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
     width: "95%"
   },
   input: {
@@ -34,20 +84,20 @@ const styles = theme => ({
   },
   iconLarge: {
     fontSize: 40,
-    marginRight: theme.spacing.unit
+    marginRight: theme.spacing(1)
   },
   leftIcon: {
     fontSize: 20,
-    marginRight: theme.spacing.unit
+    marginRight: theme.spacing(1)
   },
   rightIcon: {
     fontSize: 20,
-    marginLeft: theme.spacing.unit
+    marginLeft: theme.spacing(1)
   },
   button: {
-    marginTop: theme.spacing.unit * 2,
-    marginBottom: theme.spacing.unit * 2,
-    marginRight: theme.spacing.unit,
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+    marginRight: theme.spacing(1),
     marginLeft: 0
   }
 });
